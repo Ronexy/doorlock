7790d4466fdf3bff54438890b07fca8fe9560b79
Ronexy <yuukibrother0@gmail.com>
2023-09-09 14:43:03 +0900

    ESP32でのMQTT通信を確認

---
+++ touch/touch32/src/main.cpp
@@ -0,0 +1,60 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "SSID";
+const char* password = "PASSWORD";
+const char* mqtt_server = "mqtt.eclipseprojects.io";
+const char* topic = "TOPIC_NAME";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+  }
+  Serial.println();
+}
+
+void setup_wifi() {
+  delay(10);
+  Serial.println("Connecting to WiFi...");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+    if (client.connect("ESP32Client")) {
+      Serial.println("Connected to MQTT");
+      client.subscribe(topic);
+    } else {
+      Serial.print("Failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+}
